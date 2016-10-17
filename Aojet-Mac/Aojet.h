cf24b54e1229b885144e24e65ce6be468ddad8de
Qihe Bian <ufosky@gmail.com>
2016-10-17 17:54:32 +0800

    Add target for Mac.

---
+++ Aojet-Mac/Aojet.h
@@ -0,0 +1,19 @@
+//
+//  Aojet.h
+//  Aojet
+//
+//  Created by Qihe Bian on 10/17/16.
+//  Copyright © 2016 Qihe Bian. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Aojet.
+FOUNDATION_EXPORT double AojetVersionNumber;
+
+//! Project version string for Aojet.
+FOUNDATION_EXPORT const unsigned char AojetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aojet/PublicHeader.h>
+
+
